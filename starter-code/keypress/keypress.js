00514c17784cb2cd0626ef6e335675a12c39bd28
addykumar27 <adarshna27@yahoo.com>
2017-04-02 10:17:51 -0700

    from change to sumbit

--- starter-code/keypress/keypress.js
+++ starter-code/keypress/keypress.js
@@ -1,5 +1,24 @@
 console.log("Sanity Check: JS is working!");
+ var startTime;
+ var end Time;
+ var counting = false;
+ var repeatDisplay;
+ var total = 0;
 
 $(document).ready(function(){
+	$("window"). on("keypress", function handlekeypress(event) {
+		if (!counting) {
+			counting = true;
+			startTime = Date.now();
+			repeatDisplay = setInterval(showCurrentTotal, 100);
+			} else {
+				counting = false;
+				clearInterval( repeatDisplay);
+				endTime = (Date.now() - startTime)/1000;
+				total  = total + endTime;
+				$("#total-time"). text ( total +"seconds");
+			}
+			
+		})
 
 })
